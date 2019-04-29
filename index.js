b6e46724bf3ed80941be14f3b35a038b1036f0b6
Allan J. Martínez <Jafectnez@gmail.com>
2019-04-29 17:41:38 -0600

    arreglado listen

--- index.js
+++ index.js
@@ -127,6 +127,6 @@ function verificarAutenticacion(req, res, next) {
 }
 //Fin Verificaciones de Acceso
 
-app.listen(3333, function(){
+app.listen(process.env.PORT || 3333, function(){
     console.log("Servidor levantado");
 });
